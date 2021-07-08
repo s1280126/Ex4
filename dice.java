b40a3de608a8f1fb6f218deace5191268738310f
s1280126 <87121340+s1280126@users.noreply.github.com>
2021-07-08 19:54:23 +0900

    Update dice.java

--- dice.java
+++ dice.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class dice{
   public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String string = scanner.next();
+    System.out.println("Hello," + string + "!");
+
+    
     int ALL_SIDES = 6;
     System.out.println("Rolling the dice...");
     int x = 1 + (int) (Math.random() * ALL_SIDES);
